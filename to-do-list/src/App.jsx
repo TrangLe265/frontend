e8706f06b89174b8f6432b102c2b03fd8e3f2ac0
TrangLe265 <trang.le@myy.haaga-helia.fi>
2024-10-04 14:10:02 +0300

    adding MUI picker

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { Container, CssBaseline } from '@mui/material'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Todolist from './components/ToDoList'
+
 
 function App() {
   
   return (
-    <>
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">
+            My Todos
+          </Typography>
+        </Toolbar>
+      </AppBar>
      < Todolist />
-    </>
+    </Container>
   )
 }
 
--- to-do-list/src/components/ToDoList.jsx
+++ to-do-list/src/components/ToDoList.jsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
-import { AgGridReact } from 'ag-grid-react'; 
+import { useState, useRef } from "react";
+
+import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+
+import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css"; 
 import "ag-grid-community/styles/ag-theme-material.css"; 
 
-
 function Todolist(){
-    const [todo, setTodo] = useState({description:"", duedate: "",priority: ""}); //initialize todo with an empty string
+    const [todo, setTodo] = useState({description:"", duedate: null,priority: ""}); //initialize todo with an empty string
     const [todos, setTodos] = useState([]); //list to store all todos
+    const gridRef = useRef();   
 
     const [colDefs, setColDefs] = useState([
-        {field: "description", filter:true, editable: true}, 
-        {field: "priority", filter:true, editable: true}, 
-        {field: "duedate",  filter:true, editable: true},
-        {headerName: "Action", 
-            cellRenderer: (params) => 
-            (<button onClick = {(params) => handleDelete(params.row)}>Delete</button>)}
+        {   field: "description", filter:true, editable: true}, 
+        {   field: "priority", 
+            cellStyle: params => params.value === "High" ? { color: "red" } : { color : "black"},
+            filter:true, editable: true}, 
+        {   field: "duedate",  filter:true, editable: true},
+        /*{   headerName: "Action", 
+                cellRenderer: (params) => 
+            (<button onClick = {(params) => handleDelete(params.row)}>Delete</button>)}*/
     ]); 
 
     const handleAdd = () => {
@@ -22,53 +34,70 @@ function Todolist(){
             alert("Type all fields");
             //checking if the field is empty 
         } else {
-            setTodos([todo,...todos])//spread operation, to add new values on top of exitsing value
-            setTodo({description:"", duedate: "", priority: ""})// make the field empty after the previous task was added
+            setTodos([{ 
+                ...todo,
+                duedate: todo.duedate.toISOString()}
+                , ...todos]);//spread operation, to add new values on top of exitsing value
+            setTodo({description:"", duedate:null, priority: ""})// make the field empty after the previous task was added
         } 
     }
-    const handleDelete = (row) => {
-        
-        setTodos(todos.filter((_,index) => row !== index )) //Filter is a JVS function that returns the elements of an array that meet the condition specified in a callback function.
-        //create a new array, set that array as the new Todos array 
+    const handleDelete = () => {
+        if ((gridRef.current.getSelectedNodes()).length > 0 ){
+            //this create an array of selected nodes, however earlier we set the rowSelection to single,
+            // so the only and first item in this array is the selectied one    
+            setTodos(todos.filter((_,index) =>
+            gridRef.current.getSelectedNodes()[0].id != index)); 
+        } else {
+            alert("Select a row first")
+        } 
+    }
+
+    const changeDate = (date) =>{
+        setTodo({...todo,duedate:date})
     }
 
     return (
-        <> 
-            <h3>My Todos</h3>
-            <input
-                style = {{height:30,width:200, margin: 10}} 
-                type="text" 
-                placeholder="Description..."
-                value = {todo.description}
-                onChange={ event => setTodo({...todo, description: event.target.value})}
-            />
-             <input
-                style = {{height:30,width:100, margin: 10}} 
-                type="text" 
-                placeholder="Priority..."
-                value = {todo.priority}
-                onChange={ event => setTodo({...todo, priority: event.target.value})}
-            />
-            <input
-                style = {{height:30,width:120, margin: 10}}
-                type="date" 
-                placeholder="Due date..."
-                value = {todo.duedate}
-                onChange={ event => setTodo({...todo, duedate: event.target.value})}
-            />
+        <>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack
+                mt={2} 
+                direction="row" 
+                spacing={2}
+                justifyContent="center"
+                alignItems="center"
+            >
+                
+                <TextField
+                    label="Description"
+                    value = {todo.description}
+                    onChange={ event => setTodo({...todo, description: event.target.value})}
+                />
+                <TextField
+                    label="Priority"
+                    value = {todo.priority}
+                    onChange={ event => setTodo({...todo, priority: event.target.value})}
+                />
+                
+                <DatePicker 
+                    value = {todo.duedate} 
+                    onChange={ (date) => changeDate(date)}
+                /> 
 
-            <button onClick={handleAdd}>Add to do</button>
-        
-
+                <Button variant="contained" onClick={handleAdd}>Add to do</Button>
+                <Button variant="contained" onClick={handleDelete}>Delete</Button>
+            </Stack>
+    
             <div className="ag-theme-material" 
-                style={{height: '100%',width: '100%', backgroundColor: "black",}}>
+                style={{height: '400px',width: '100%', backgroundColor: "black",alignContent:'center'}}>
                 <AgGridReact
+                    ref = {gridRef}
+                    onGridReady={params => gridRef.current = params.api }
+                    rowSelection="single"
                     rowData = {todos}
                     columnDefs= {colDefs}
                 />
             </div>
-            
-           
+            </LocalizationProvider>
         </>
     )
 }
